717d0aef1cbdcdabf0efdfc98be6323ea4928c22
blazerpaul <blazerpaul15@gmail.com>
2022-02-12 21:26:31 +0800

    violet: Add prebuilt Graphene Camera
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: I15e968bc6db781abbbf08a990996b16a2ac31aab

--- device.mk
+++ device.mk
@@ -138,6 +138,9 @@ PRODUCT_PACKAGES += \
     libxml2 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    GrapheneCamera
+
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so
 
