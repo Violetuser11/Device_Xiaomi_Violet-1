294592cbf1fc759a8e2f8f53eeb70ff7e0c4ef33
kdrag0n <danny@kdrag0n.dev>
2022-12-06 11:20:07 +0530

    violet: Add Repainter ROM integration

--- device.mk
+++ device.mk
@@ -382,6 +382,10 @@ PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
     PresencePolling \
     RcsService
+    
+# Repainter integration
+PRODUCT_PACKAGES += \
+    RepainterServicePriv
 
 # RIL
 PRODUCT_PACKAGES += \
